5a5980009419997d570b809feb09c879815700f5
PotOfCoffee2Go <PotOfCoffee2Go@users.noreply.github.com>
2017-01-30 20:35:24 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const http         = require('http'),
+      fs           = require('fs'),
+      path         = require('path'),
+      contentTypes = require('./utils/content-types'),
+      sysInfo      = require('./utils/sys-info'),
+      uuid         = require('node-uuid'),
+      env          = process.env;
+
+var version = JSON.parse(fs.readFileSync(path.join('', './package.json'))).version;
+
+/// ---------- API Server
+var server = http.createServer(function (req, res) {
+  var url = req.url;
+  if (url == '/') {
+    url += 'index.html';
+  }
+
+  // Health monitoring
+
+  if (url == '/health') {
+    res.writeHead(200);
+    res.end();
+  }
+  else if (url.indexOf('/version/') == 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Cache-Control', 'no-cache, no-store');
+    res.end(JSON.stringify( {version: version} ));
+  }
+  else {
+    fs.readFile('./static' + url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end();
+      }
+      else {
+        var ext = path.extname(url).slice(1);
+        if (ext && ext.length < 1) {
+          ext = 'txt';
+        }
+        res.setHeader('Content-Type', contentTypes[ext]);
+        if (ext === 'html') {
+          res.setHeader('Cache-Control', 'no-cache, no-store');
+        }
+        res.end(data);
+      }
+    });
+  }
+});
+
+/// ---------- WebSocket Server
+var ios = require('socket.io')(server);
+
+server.listen(env.OPENSHIFT_NODEJS_PORT || 3000, env.OPENSHIFT_NODEJS_IP || 'localhost', function () {
+    // When a client connects
+    ios.on('connection', function (socket) {
+
+        /// #### Events
+        socket.on('connected', onConnected);
+
+        socket.on('disconnect', onDisconnect);
+
+        socket.on('update bid', onUpdateBid);
+
+        /// #### Connected message
+        /// - Upon connection send a connected message to the client
+        ///   - In the message will be a newClientId which the client
+        ///     uses in the response
+        ///   - If the client disconnects - when it reconnects it should
+        ///     ignore newClientID and use the same clientId that was given
+        ///     during the first connect
+        /// - Send ClientId to client
+        emitConnected();
+
+        function emitConnected() {
+            // Send connected - with a recommended session/client id
+            // When the client responds it can use these values or
+            //  previous values if reconnecting - see onConnected()
+            socket.emit('connected', {
+              data: {},
+              error: null
+            });
+        }
+
+        /// - Got Connected response from client
+        ///   - Activate client on the server
+        function onConnected(msg) {
+            if (msg.data && msg.data.room) {
+              socket.join(msg.data.room);
+              console.log("onConnected: joined room - " + JSON.stringify(msg));
+            }
+            else {
+              console.log('onConnected: ' + JSON.stringify(msg));
+            }
+        }
+
+        /// #### Disconnect message
+        /// - Tell server to deactivate client
+        function onDisconnect() {
+              console.log('onDisconnect: ' + socket.id);
+        }
+
+        /// - A bid has been updated
+        function onUpdateBid(msg) {
+          if (msg.data && msg.data.room) {
+            socket.broadcast.to(msg.data.room).emit('reload', msg);
+            console.log('onUpdateBid: broadcast - ' + JSON.stringify(msg));
+          }
+          console.log('onUpdateBid: %s',JSON.stringify(msg));
+        }
+    
+    });
+  console.log('Application worker %s started...', process.pid);
+});
+
+
+/*
+/// ---------- API Server connection to websockets
+var ioc = require('socket.io-client'),
+csocket = ioc.connect('http://localhost:3000', {reconnect: true});
+
+csocket.on('connect', function () {
+    console.log('API Server Connect');
+});
+csocket.on('disconnect', function () {
+    changeIoIndicator('red');
+});
+
+csocket.on('connected', function (msg) {
+  console.log('API Server Connected: ' + JSON.stringify(msg));
+  emitConnected(msg);
+});
+
+// Export variables and functions
+function emitConnected(msg) {
+    csocket.emit('connected', msg);
+    changeIoIndicator('green');
+}
+
+// -----------------------
+
+function changeIoIndicator(color) {
+    if (color === 'red') {
+      console.log('Red')
+    }
+    else if (color === 'green') {
+      console.log('Green')
+    }
+}
+*/

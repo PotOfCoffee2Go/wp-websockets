80e1d5ee46d8a59194668dd740b731d313a97b81
PotOfCoffee2Go <kim@lrunit.net>
2017-02-18 05:00:14 +0000

    Cleaned up whitespace tabs to spaces

--- wordpress/plugins/ua-dynamic-update/js/client.js
+++ wordpress/plugins/ua-dynamic-update/js/client.js
@@ -1,63 +1,61 @@
 function uadu_websocket(room) {
-	this.socket = null;
-	this.room = room;
-	this.data = {};
-	
-	this.start();
-}
+this.socket = null;
+this.room = room;
+this.data = {};
 
-	
+this.start();
+};
+
+
 uadu_websocket.prototype.start = function start() {
-	var self = this;
-	this.socket = io.connect('https://wp-websockets-potofcoffee2go.c9users.io');
-	
-	this.socket.on('disconnect', function () {
-		self.changeIoIndicator('red');
-	});
-	this.socket.on('Connected', function (msg) {
-		console.log('OnConnected: ' + JSON.stringify(msg));
-		self.emitJoin();
-	});
- 	this.socket.on('reload', function (msg) {
-		var x = document.getElementsByClassName("wdm_bidding_price");
-		for (var i = 0; i < x.length; i++) {
-		    x[i].innerHTML = '<strong>$' + (Number(msg.data.ab_bid).toFixed(2)) + ' USD</strong>';
-		}
-	});
-}
- 
-uadu_websocket.prototype.emitJoin = function emitJoin() {
-	this.socket.emit('Join',
-	        {
-	        	room: this.room,
-	            data: this.data,
-	            error: null
-	        });
-	this.changeIoIndicator('green');
-}
-	
-uadu_websocket.prototype.emitUpdateBid = function emitUpdateBid(data) {
-	this.socket.emit('update bid',
-	        {
-				room: data.auction_id,
-				received: null,
-				broadcast: null,
-	            data: data,
-	            error: null
-	        });
-}
-	
-	// -----------------------
-	
-uadu_websocket.prototype.changeIoIndicator = function changeIoIndicator(color) {
-	if (color === 'red') {
-		document.querySelector('#redio-img').style.display = 'inline-block';
-		document.querySelector('#greenio-img').style.display = 'none';
-	}
-	else if (color === 'green') {
-		document.querySelector('#redio-img').style.display = 'none';
-		document.querySelector('#greenio-img').style.display = 'inline-block';
-	}
-}
-	
+    var self = this;
+    this.socket = io.connect('https://wp-websockets-potofcoffee2go.c9users.io');
+    
+    this.socket.on('disconnect', function () {
+        self.changeIoIndicator('red');
+    });
+    this.socket.on('Connected', function (msg) {
+        console.log('OnConnected: ' + JSON.stringify(msg));
+        self.emitJoin();
+    });
+    this.socket.on('reload', function (msg) {
+        var x = document.getElementsByClassName("wdm_bidding_price");
+        for (var i = 0; i < x.length; i++) {
+            x[i].innerHTML = '<strong>$' + (Number(msg.data.ab_bid).toFixed(2)) + ' USD</strong>';
+        }
+    });
+};
 
+uadu_websocket.prototype.emitJoin = function emitJoin() {
+    this.socket.emit('Join',
+        {
+            room: this.room,
+            data: this.data,
+            error: null
+        });
+    this.changeIoIndicator('green');
+};
+  
+uadu_websocket.prototype.emitUpdateBid = function emitUpdateBid(data) {
+    this.socket.emit('update bid',
+        {
+            room: data.auction_id,
+            received: null,
+            broadcast: null,
+            data: data,
+            error: null
+        });
+};
+
+  // -----------------------
+  
+uadu_websocket.prototype.changeIoIndicator = function changeIoIndicator(color) {
+    if (color === 'red') {
+        document.querySelector('#redio-img').style.display = 'inline-block';
+        document.querySelector('#greenio-img').style.display = 'none';
+    }
+    else if (color === 'green') {
+        document.querySelector('#redio-img').style.display = 'none';
+        document.querySelector('#greenio-img').style.display = 'inline-block';
+    }
+};
--- wordpress/plugins/ua-dynamic-update/ua-dynamic-update.php
+++ wordpress/plugins/ua-dynamic-update/ua-dynamic-update.php
@@ -20,12 +20,12 @@ register_deactivation_hook(__FILE__, 'fwds_slider_deactivation');
 /* Javascript */
 function uadu_scripts() {
 
-  wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery');
 
-  wp_enqueue_script('socketio', '//cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min.js');
+    wp_enqueue_script('socketio', '//cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.min.js');
 
-  wp_register_script('uadu_client', plugins_url('js/client.js', __FILE__),array("socketio"));
-  wp_enqueue_script('uadu_client');
+    wp_register_script('uadu_client', plugins_url('js/client.js', __FILE__),array("socketio"));
+    wp_enqueue_script('uadu_client');
 
 }
 add_action('wp_enqueue_scripts', 'uadu_scripts');

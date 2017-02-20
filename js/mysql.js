7ad1387a65aef45683b77ddf73cece991b632be5
PotOfCoffee2Go <kim@lrunit.net>
2017-02-20 20:16:24 +0000

    Add SQL to get taxonomy/category list by item

--- js/mysql.js
+++ js/mysql.js
@@ -18,10 +18,10 @@
             
             
             if (action.urlparts[3] === 'auctions' ) {
-                connect();
-
                 var aucId = action.urlparts[4];
-                connection.query('select * from wp_posts' + (aucId ? (' where id=' + aucId) : ''), function (perror, presults, pfields) {
+                connect();
+                connection.query('select * from wp_posts where post_type = "ultimate-auction" ' + 
+                        (aucId ? (' and id=' + aucId) : ''), function (perror, presults, pfields) {
                     if (perror) throw perror;
                     if (presults.length === 0) {
                         connection.end();
@@ -32,13 +32,20 @@
                     var auctions = [];
 
                     async.forEachOf(presults, function (post, key, callback) {
-                        if (post.post_type === 'ultimate-auction') {
-                            connection.query('select * from wp_wdm_bidders where auction_id=' + post.ID, function (berror, bresults, bfields) {
-                                if (berror) console.log(berror);
-                                connection.query('select * from wp_postmeta where post_id=' + post.ID, function (merror, mresults, mfields) {
-                                    if (merror) console.log(merror);
-                                    connection.query('select * from wp_comments where comment_post_id=' + post.ID, function (cerror, cresults, cfields) {
-                                        if (cerror) console.log(cerror);
+                        connection.query('select wp_wdm_bidders.*, wp_users.user_login, wp_users.display_name from wp_wdm_bidders ' +
+                                'left join wp_users on wp_wdm_bidders.email = wp_users.user_email ' +
+                                'where auction_id=' + post.ID, function (berror, bresults, bfields) {
+                            if (berror) callback(berror);
+                            connection.query('select * from wp_postmeta where post_id=' + post.ID, function (merror, mresults, mfields) {
+                                if (merror) callback(merror);
+                                connection.query('select * from wp_comments where comment_post_id=' + post.ID, function (cerror, cresults, cfields) {
+                                    if (cerror) callback(cerror);
+                                    var toptions = {nestTables: false, sql: 'select * from wp_term_relationships ' +
+                                        'left join wp_terms on wp_term_relationships.term_taxonomy_id = wp_terms.term_id ' +
+                                        'left join wp_term_taxonomy on wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_id ' +
+                                        'where wp_term_relationships.object_id=' + post.ID};
+                                    connection.query(toptions, function (terror, tresults, tfields) {
+                                        if (terror) callback(terror);
                                         
                                         var metadata = {};
                                         mresults.forEach(function(element) {
@@ -46,6 +53,8 @@
                                         });
             
                                         post.meta =  metadata;
+                                        post.meta.ryan = 'hi!!!';
+                                        post.categories =  tresults;
                                         post.comments =  cresults;
                                         post.bidders =  bresults;
                                         auctions.push(post);
@@ -53,10 +62,7 @@
                                     });
                                 });
                             });
-                        }
-                        else {
-                            callback();
-                        }
+                        });
                     }, function (err) {
                         if (err) throw err;
                         connection.end();
@@ -76,6 +82,24 @@
                 });
             }
             
+
+            else if (action.urlparts[3] === 'test' ) {
+                aucId = action.urlparts[4];
+                var options = {nestTables: false};
+                options.sql = 'select * from wp_posts ' + 
+                    'LEFT JOIN wp_wdm_bidders ON wp_posts.ID = wp_wdm_bidders.auction_id ' +
+                    'LEFT JOIN wp_postmeta ON wp_posts.ID = wp_postmeta.post_id ' +
+                    'LEFT JOIN wp_comments ON wp_posts.ID = wp_comments.comment_post_id ' +
+                    (aucId ? (' where wp_posts.id=' + aucId) : '');
+                connect();
+                connection.query(options, function (error, results, fields) {
+                    if (error) throw error;
+                    connection.end();
+                    if (cb) cb(null, {auctions: results});
+                });
+            }
+
+            
         },
 
     };
@@ -84,6 +108,10 @@
         connection = mysql.createConnection(cfg.mysql);
         // connection.connect();
     }
+
+
+
+
 
 })();
 
